4a9995e4f07a02491bb612602a0cfcb7a22ee2c3
azabirnik <106436338+azabirnik@users.noreply.github.com>
2022-12-22 07:22:27 +0300

    more comments

--- cli.py
+++ cli.py
@@ -4,25 +4,32 @@ from ruamel.yaml import safe_load
 
 
 def _log(fmt_str: str):
+    """Создает декоратор, который логгирует время выполнения функций пиццерии"""
+
     def decorator(function):
         def wrapper(*args, **kwargs):
             time = randint(1, 5)
             print(fmt_str.format(time))
             return function(*args, **kwargs)
+
         return wrapper
+
     return decorator
 
 
 class Pizza:
 
-    """ A class for Pizza """
+    """Рецепт пиццы"""
 
-    def __init__(self, pizza: dict):
+    def __init__(self, pizza: dict, xl: bool):
+        """Объект класса создается на основе словаря, который парсится из yaml"""
         self.name = pizza["pizza"].lower()
         self.emoji = pizza["emoji"]
         self.ingredients = pizza["ingredients"]
+        self.xl = xl
 
     def dict(self):
+        """Конвертирует объект класса обратно в словарь"""
         return {
             "pizza": self.name,
             "emoji": self.emoji,
@@ -30,34 +37,49 @@ class Pizza:
         }
 
     def __str__(self):
+        """Строка в меню пиццерии"""
         return f'- {self.name.title()} {self.emoji} : {", ".join(self.ingredients)}'
 
     def __ed__(self, second):
+        """Считаем что пиццы совпадают если их имена, размеры и составы совпадают"""
         if self.name != second.name:
             return False
-        # pass emoji
-        return sorted(self.ingredients) == sorted(second.ingredients)
+        # Эмоджи игнорируем
+        return (
+            sorted(self.ingredients) == sorted(second.ingredients)
+            and self.xl == second.xl
+        )
 
     @_log("🛵  Доставили за {}с!")
     def delivery(self):
+        """Здесь должна быть описана логика доставки пиццы"""
         pass
 
     @_log("🏠  Забрали за {}с!")
     def pickup(self):
+        """Здесь должна быть описана логика забора пиццы из пиццерии"""
         pass
 
     @_log("👩‍🍳  Приготовили за {}с!")
     def bake(self, xl: bool):
+        """Процедура приготовления пиццы"""
         pass
 
 
 def load_menu(source="pizza.yml"):
+    """
+    Загружает меню пиццерии из исходного yml-файла.
+    Возвращает словать <элемент меню>: <рецепт>
+    <рецепт> -- как элемент соответствующего класса.
+    В настоящей версии реализован только класс пицц.
+    """
     with open(source, "rt") as f:
         loaded_menu = safe_load(f)["menu"]
     return {item["pizza"]: Pizza(item) for item in loaded_menu}
 
 
 def show_menu():
+    """Демонстрирует доступное для заказа меню"""
     loaded_menu = load_menu()
     for pizza in loaded_menu.values():
         print(pizza)
@@ -65,6 +87,7 @@ def show_menu():
 
 @click.group()
 def cli():
+    """Техническая функция для инициализации группы декораторов cli"""
     pass
 
 
@@ -74,7 +97,12 @@ def cli():
 @click.option("--XL", default=False, is_flag=True)
 @click.argument("pizza", nargs=1)
 def order(pizza: str, delivery: bool, l: bool, xl: bool):
-    """Готовит и доставляет пиццу"""
+    """
+    Готовит и доставляет пиццу, если указан флаг delivery.
+    Если пиццы нет -- выведет сообщение об этом и доступное меню.
+    Если заказаная пицца должна быть одновременно L и XL --
+    укажет на противоречие в заказе.
+    """
     pizza_order = load_menu().get(pizza.lower())
     if not pizza_order:
         print(f'Такой пиццы в меню нет - "{pizza}". Доступны для заказа:')
